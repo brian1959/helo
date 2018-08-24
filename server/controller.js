dc98015cd29bec062d32ee97a1783ce425e426de
brian1959 <brian@solutionation.net>
2018-08-24 15:44:36 -0600

    login register working

--- server/controller.js
+++ server/controller.js
@@ -2,13 +2,15 @@ module.exports = {
   addUser: async (req, res, next) => {
     const dbInstance = req.app.get("db");
     const { husername, hpassword } = req.body;
+    console.log(req.body);
     let foundUser = await dbInstance.find_user([husername]);
     if (foundUser[0]) {
       req.session.user = foundUser[0];
-      res.redirect("/#/dashboard");
+      res.status(200).send(req.session.user);
     } else {
       let createdUser = await dbInstance.add_user([husername, hpassword]);
-      req.session.user = createdUser[0] = res.redirect("/#/dashboard");
+      req.session.user = createdUser[0];
+      res.status(200).send(req.session.user);
     }
   }
 };
--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -7,8 +7,8 @@ class Auth extends Component {
     super(props);
 
     this.state = {
-      username: "",
-      password: ""
+      husername: "",
+      hpassword: ""
     };
   }
 
@@ -17,11 +17,12 @@ class Auth extends Component {
       husername: this.state.husername,
       hpassword: this.state.hpassword
     };
-
+    console.log(newUser);
     axios
       .post("/api/user", newUser)
       .then(response => {
         console.log(response);
+        this.props.history.push("/dashboard");
       })
       .catch(err => {
         console.log(err);
@@ -39,13 +40,13 @@ class Auth extends Component {
           <div className="auth-input">
             <input
               placeholder="username"
-              onChange={e => this.setState({ username: e.target.value })}
-              value={this.state.username}
+              onChange={e => this.setState({ husername: e.target.value })}
+              value={this.state.husername}
             />
             <input
               palceholder="password"
-              onChange={e => this.setState({ password: e.target.value })}
-              value={this.state.password}
+              onChange={e => this.setState({ hpassword: e.target.value })}
+              value={this.state.hpassword}
             />
           </div>
         </div>
